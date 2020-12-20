5500ebe4458282e734e864a899a4b79affc9b4b7
vaguila95 <vlaguila@uc.cl>
2020-12-20 01:04:19 -0300

    ✨ fetch pokemon

--- src/components/pokemonCard/pokemonCard.js
+++ src/components/pokemonCard/pokemonCard.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
 const PokemonCard = (props) => {
+  const { id } = props; 
+  const [pokemonData, setPokemonData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const data = await response.json();
+      setPokemonData(data);
+    }
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+
+  }, [pokemonData])
+
+  const cardContent = () => {
+    if (pokemonData) {
+      return (
+        <h1>#{id} {pokemonData.name}</h1>
+      )
+    }
+    else {
+      return (
+        <h1>Loading...</h1>
+      )
+    }
+  }
+
   return(
     <div>
-      <h1>Pokemon</h1>
+      {cardContent()}
     </div>
   )
 }
