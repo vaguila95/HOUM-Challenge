799c3b3bde34d1cbc8aa0a8e81c5515bc170f178
vaguila95 <vlaguila@uc.cl>
2020-12-20 01:03:47 -0300

    🚚 file structure

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import PokemonCard from '../pokemonCard/pokemonCard'
+
+const App = () => {
+
+  const idArray = []
+  const pageSize = 120
+  let auxId = 1
+  while (idArray.length < pageSize) {
+    idArray.push(auxId)
+    auxId += 1;
+  }
+
+  return (
+    <div className="App">
+      <h1>PokeCards</h1>
+      {idArray.map((pokemonId) => (
+        <PokemonCard key={pokemonId} id={pokemonId} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pokemonCard/pokemonCard.js
@@ -0,0 +1,9 @@
+const PokemonCard = (props) => {
+  return(
+    <div>
+      <h1>Pokemon</h1>
+    </div>
+  )
+}
+
+export default PokemonCard;
