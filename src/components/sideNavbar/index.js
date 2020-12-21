b598d313c25da872f18a88f314dcdd06cbd6e07c
vaguila95 <vlaguila@uc.cl>
2020-12-21 00:02:50 -0300

    ✨ fixed sidebar

--- src/components/sideNavbar/index.js
+++ src/components/sideNavbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './sideNavbar.less'
-import { Button, Menu } from "antd"
+import { Affix, Button, Menu } from "antd"
 import Sider from "antd/lib/layout/Sider"
 import { useState } from 'react'
 import {
@@ -15,24 +15,27 @@ const SideNavbar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Sider 
-      theme="light"
-      collapsible={true}
-    >
-      <Button type="primary" id="collapse-button" onClick={() => setCollapsed(!collapsed)}>
-        {collapsed ? <DoubleRightOutlined /> : <DoubleLeftOutlined />}
-      </Button>
-      {/* <Input id="search-bar" placeholder="Ingresa tu búsqueda" /> */}
-      <Menu
-        mode="inline"
+    <Affix>
+      <Sider 
         theme="light"
-        inlineCollapsed={collapsed}
+        collapsible={true}
+        style={{ height: '100vh'}}
       >
-        <SubMenu key="sub1" id="filter-menu" icon={<FilterFilled id="filter-icon"/>} title="FILTRAR TIPOS">
-          <Menu.Item key="1">Select</Menu.Item>
-        </SubMenu>
-      </Menu>
-    </Sider>
+        <Button type="primary" id="collapse-button" onClick={() => setCollapsed(!collapsed)}>
+          {collapsed ? <DoubleRightOutlined /> : <DoubleLeftOutlined />}
+        </Button>
+        {/* <Input id="search-bar" placeholder="Ingresa tu búsqueda" /> */}
+        <Menu
+          mode="inline"
+          theme="light"
+          inlineCollapsed={collapsed}
+        >
+          <SubMenu key="sub1" id="filter-menu" icon={<FilterFilled id="filter-icon"/>} title="FILTRAR TIPOS">
+            <Menu.Item key="1">Select</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+    </Affix>
   )
 }
 
