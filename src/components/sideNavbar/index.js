e5bf8bdae79efb3cb762b768e0d1b9e1e34e078c
vaguila95 <vlaguila@uc.cl>
2020-12-20 21:36:26 -0300

    ✨ sidebar

--- src/components/sideNavbar/index.js
+++ src/components/sideNavbar/index.js
@@ -1,16 +1,32 @@
+import React from 'react'
+import './sideNavbar.less'
 import { Button, Menu } from "antd"
 import Sider from "antd/lib/layout/Sider"
+import { useState } from 'react'
+import {
+  FilterFilled
+} from '@ant-design/icons';
+
+const { SubMenu } = Menu;
 
 const SideNavbar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <Sider>
-      <Menu>
-        <Menu.Item>
-          <form className="pagesize-form">
-            <input className="pagesize-bar" type="text"/>
-            <Button className="pagesize-button" type="primary">Submit</Button>
-          </form>
-        </Menu.Item>
+    <Sider id="sidebar">
+      <Button type="primary" onClick={() => setCollapsed(!collapsed)}>
+        
+      </Button>
+      <Menu
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={collapsed}
+      >
+        <SubMenu key="1" icon={<FilterFilled />} title="FILTRAR TIPOS">
+          <Menu.Item key="5">Select</Menu.Item>
+        </SubMenu>
       </Menu>
     </Sider>
   )
